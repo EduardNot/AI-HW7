796e83bff77c4a4f20b0df4ffae79b8a43c0a449
Eduard Rudi <eduard.rudi@inbox.ru>
2020-12-17 17:34:32 +0200

    Now removes pasted pipes

--- game.py
+++ game.py
@@ -34,12 +34,21 @@ def draw_pipe(pipes):
         screen.blit(PIPE_IMG_REV, pipe[1])
 
 
+def remove_pipe(pipes):
+    pipes_copy = pipes.copy()
+    for i in range(len(pipes)):
+        if pipes[i][0].centerx < -50:
+            pipes_copy.pop(i)
+    return pipes_copy
+
+
 if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     clock = pygame.time.Clock()
 
     BIRD_IMG = pygame.transform.scale(pygame.image.load('assets/bird1.png'), (51, 36)).convert()
+    # BIRD_IMG = pygame.transform.scale2x(pygame.image.load('assets/bird1.png')).convert()
     BG_IMG = pygame.transform.scale2x(pygame.image.load('assets/bg.png')).convert()
     BASE_IMG = pygame.transform.scale2x(pygame.image.load('assets/base.png')).convert()
     BIRD_RECT = BIRD_IMG.get_rect(center=(100, 325))
@@ -63,6 +72,7 @@ if __name__ == '__main__':
                     BIRD_MOVEMENT = 0
                     BIRD_MOVEMENT -= 8
             if event.type == SPAWNPIPE:
+                print(len(pipe_list))
                 pipe_list.append(create_pipe())
 
         screen.blit(BG_IMG, (0, 0))
@@ -73,6 +83,7 @@ if __name__ == '__main__':
 
         pipe_list = move_pipe(pipe_list)
         draw_pipe(pipe_list)
+        pipe_list = remove_pipe(pipe_list)
 
         BASE_X_POS -= 1
         draw_base()
