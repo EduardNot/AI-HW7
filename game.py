0e90d2954caa60d10aa77aa45f70cdb8f7661e84
Eduard Rudi <eduard.rudi@inbox.ru>
2020-12-18 14:41:28 +0200

    Moved pipes to class

--- game.py
+++ game.py
@@ -4,42 +4,44 @@
 import pygame
 import sys
 import random
+import copy
 
 WIN_WIDTH = 575
 WIN_HEIGHT = 800
 
 
+class Pipes:
+    pipe_list = []
+    gap = 225
+
+    def add(self):
+        height = random.randint(300, 625)
+        pipe_bottom = PIPE_IMG.get_rect(midtop=(700, height))
+        pipe_top = PIPE_IMG.get_rect(midbottom=(700, height - self.gap))
+        self.pipe_list.append((pipe_bottom, pipe_top))
+
+    def move(self):
+        for pipe in self.pipe_list:
+            pipe[0].centerx -= 5
+            pipe[1].centerx -= 5
+        self.draw()
+
+    def draw(self):
+        for pipe in self.pipe_list:
+            screen.blit(PIPE_IMG, pipe[0])
+            screen.blit(PIPE_IMG_REV, pipe[1])
+
+    def remove_pipe(self):
+        pipes_new = []
+        for i in range(len(self.pipe_list)):
+            if self.pipe_list[i][0].centerx > -50:
+                pipes_new.append(self.pipe_list[i])
+        self.pipe_list = copy.deepcopy(pipes_new)
+
+
 def draw_base():
     screen.blit(BASE_IMG, (BASE_X_POS, 700))
     screen.blit(BASE_IMG, (BASE_X_POS + 575, 700))
-
-
-def create_pipe():
-    height = random.randint(300, 625)
-    pipe_bottom = PIPE_IMG.get_rect(midtop=(700, height))
-    pipe_top = PIPE_IMG.get_rect(midbottom=(700, height - 225))
-    return tuple((pipe_bottom, pipe_top))
-
-
-def move_pipe(pipes):
-    for pipe in pipes:
-        pipe[0].centerx -= 5
-        pipe[1].centerx -= 5
-    return pipes
-
-
-def draw_pipe(pipes):
-    for pipe in pipes:
-        screen.blit(PIPE_IMG, pipe[0])
-        screen.blit(PIPE_IMG_REV, pipe[1])
-
-
-def remove_pipe(pipes):
-    pipes_copy = pipes.copy()
-    for i in range(len(pipes)):
-        if pipes[i][0].centerx < -50:
-            pipes_copy.pop(i)
-    return pipes_copy
 
 
 if __name__ == '__main__':
@@ -54,9 +56,10 @@ if __name__ == '__main__':
     BIRD_RECT = BIRD_IMG.get_rect(center=(100, 325))
     PIPE_IMG = pygame.transform.scale2x(pygame.image.load('assets/pipe.png')).convert()
     PIPE_IMG_REV = pygame.transform.flip(PIPE_IMG, False, True).convert()
-    pipe_list = []
+
     SPAWNPIPE = pygame.USEREVENT
     pygame.time.set_timer(SPAWNPIPE, 1200)
+    pipes = Pipes()
 
     GRAVITY = 0.25
     BIRD_MOVEMENT = 0
@@ -72,8 +75,7 @@ if __name__ == '__main__':
                     BIRD_MOVEMENT = 0
                     BIRD_MOVEMENT -= 8
             if event.type == SPAWNPIPE:
-                print(len(pipe_list))
-                pipe_list.append(create_pipe())
+                pipes.add()
 
         screen.blit(BG_IMG, (0, 0))
 
@@ -81,9 +83,8 @@ if __name__ == '__main__':
         BIRD_RECT.centery += BIRD_MOVEMENT
         screen.blit(BIRD_IMG, BIRD_RECT)
 
-        pipe_list = move_pipe(pipe_list)
-        draw_pipe(pipe_list)
-        pipe_list = remove_pipe(pipe_list)
+        pipes.move()
+        pipes.remove_pipe()
 
         BASE_X_POS -= 1
         draw_base()
