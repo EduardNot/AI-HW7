dacc26688d46c3c6988668af63c8dcda1e89830b
Artjom <valart99@gmail.com>
2020-12-30 21:38:06 +0200

    Adding bird animation (wing swinging)

--- Bird.py
+++ Bird.py
@@ -1,17 +1,30 @@
+import pygame
+
 class Bird:
     GRAVITY = 0.25
     BIRD_MOVEMENT = 0
     BIRD_IMG = None
     BIRD_RECT = None
     JUMP_HEIGHT = 6
+    TIMING = 0
 
     def __init__(self, BIRD_IMG, BIRD_RECT):
         self.BIRD_IMG = BIRD_IMG
         self.BIRD_RECT = BIRD_RECT
 
+    def animation(self):
+        if self.TIMING%30 == 0:
+            self.BIRD_IMG = pygame.transform.scale2x(pygame.image.load('assets/bird3.png')).convert()
+        elif self.TIMING%20 == 0:
+            self.BIRD_IMG = pygame.transform.scale2x(pygame.image.load('assets/bird2.png')).convert()
+        elif self.TIMING%10 == 0:
+            self.BIRD_IMG = pygame.transform.scale2x(pygame.image.load('assets/bird1.png')).convert()
+        self.TIMING += 1
+
     def move(self):
         self.BIRD_MOVEMENT += self.GRAVITY
         self.BIRD_RECT.centery += self.BIRD_MOVEMENT
+        self.animation()
 
     def draw(self, screen):
         screen.blit(self.BIRD_IMG, self.BIRD_RECT)
